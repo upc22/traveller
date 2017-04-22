76eeb60e9073b7ae810716077a2e8c94a55599ab
SAPIENT\pgoe12 <pgoel12@sapient.com>
2017-04-22 19:22:20 +0530

    Maps cleaned, form builder removed

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AgmCoreModule } from '@agm/core';
 
@@ -11,6 +12,7 @@ import { MapComponent } from './map/map.component';
   imports: [
     CommonModule,
     AgmCoreModule,
+    FormsModule,
     MapsRoutingModule
   ],
   declarations: [ MapComponent ]
