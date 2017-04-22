3de6bea3b5da7494f73b688a6d0c830f837a48c9
Vidit Shah <vshah4@sapient.com>
2017-04-22 15:13:43 +0530

    * commenting routing for time being

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,12 @@
 
 import { Routes } from '@angular/router';
-import { LoginComponent } from "app/login/login.component";
+import { LoginComponent } from 'app/login/login.component';
 
 export const AppRoutes: Routes = [{
   path: '',
   component: LoginComponent,
-  children: [{
-    path: '',
-    loadChildren: './maps/maps.module#MapsModule'
-  }]
+  // children: [{
+  //   path: '',
+  //   loadChildren: './maps/maps.module#MapsModule'
+  // }]
 }];
