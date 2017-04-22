71769ae182a2817b2cded16cd110e2ff70cdd4a6
uprata <upratap@sapient.com>
2017-04-22 18:42:58 +0530

    note comp

---
+++ src/app/shared/notes/notes-routing.module.ts
@@ -0,0 +1,16 @@
+import { NoteComponent } from './note/note.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: NoteComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class NotesRoutingModule { }
