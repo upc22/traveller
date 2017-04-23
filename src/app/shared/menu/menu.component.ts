8f90ce418fb36eb827b67cd79c6f2dd4a264f3df
Vidit Shah <vshah4@sapient.com>
2017-04-24 05:07:25 +0530

    * logout

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AngularFire } from 'angularfire2';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-menu',
@@ -9,7 +11,7 @@ export class MenuComponent implements OnInit {
 
   @Output() toggleNav = new EventEmitter<boolean>();
   flag: boolean = false;
-  constructor() { }
+  constructor(private af: AngularFire, private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,6 +24,14 @@ export class MenuComponent implements OnInit {
       this.flag = true;
       this.toggleNav.emit(this.flag);
     }
+  }
+
+  logOut() {
+    this.af.auth.logout().then(() => {
+      localStorage.clear();
+      sessionStorage.clear();
+      this.router.navigate(['/']);
+    });
   }
 
 }
